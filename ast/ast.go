8abbf0e076fb7851edfd5d225244b4e4835ac5e3
Sky Hampton <forrest.hampton744@myci.csuci.edu>
2026-03-29 16:02:41 -0700

    Added integer literal parsing

--- ast/ast.go
+++ ast/ast.go
@@ -102,3 +102,12 @@ func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
 func (es *ExpressionStatement) String() string {
 	return es.Expression.String()
 }
+
+type IntegerLiteral struct {
+	Token token.Token
+	Value int64
+}
+
+func (il *IntegerLiteral) ExpressionNode()      {}
+func (il *IntegerLiteral) TokenLiteral() string { return il.Token.Literal }
+func (il *IntegerLiteral) String() string       { return il.Token.Literal }
--- parser/parser.go
+++ parser/parser.go
@@ -41,6 +41,7 @@ import (
 	"learningLanguage/ast"
 	"learningLanguage/lexer"
 	"learningLanguage/token"
+	"strconv"
 )
 
 const (
@@ -80,6 +81,7 @@ func New(l *lexer.Lexer) *Parser {
 
 	p.prefixParseFns = make(map[token.TokenType]prefixParseFn)
 	p.registerPrefix(token.IDENT, p.parseIdentifier)
+	p.registerPrefix(token.NUMBER, p.parseIntegerLiteral)
 
 	return p
 }
@@ -214,4 +216,19 @@ func (p *Parser) parseExpression(precedence int) ast.Expression {
 
 func (p *Parser) parseIdentifier() ast.Expression {
 	return &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+}
+
+func (p *Parser) parseIntegerLiteral() ast.Expression {
+	lit := &ast.IntegerLiteral{Token: p.curToken}
+
+	value, err := strconv.ParseInt(p.curToken.Literal, 0, 64)
+	if err != nil {
+		msg := fmt.Sprintf("could not parse %q as integer", p.curToken.Literal)
+		p.errors = append(p.errors, msg)
+		return nil
+	}
+
+	lit.Value = value
+
+	return lit
 }
